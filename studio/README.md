7f0b5350c14d274319f392f2efd4da09c512fba9
Sanity.io <hello@sanity.io>
2020-07-06 18:04:16 +0200

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+# plynpiesnimoja-studio
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'spwn81yk',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
