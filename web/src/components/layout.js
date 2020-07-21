899756fdb78b2724c4b50a760e52d619237bf846
dominicom <dominik.kiepuszewski@gmail.com>
2020-07-21 10:32:10 +0200

    Added google Analytics plugin

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from 'src/components/header'
 
 import Footer from 'src/components/footer'
 
+//import { PolishBreakingText } from 'src/lib/string-utils'
+
 import 'src/styles/layout.scss'
 import styles from './layout.module.scss'
 
-const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => (
-  <>
-    <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
-    <Main children={children} />
-    <Footer />
-  </>
-)
+const Layout = ({ children, onHideNav, onShowNav, showNav, siteTitle }) => {
+  //useEffect(() => PolishBreakingText(), [])
+  return(
+    <>
+      <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
+      <Main children={children} />
+      <Footer />
+    </>
+  )
+}
 
 export default Layout
 
--- web/src/lib/string-utils.js
+++ web/src/lib/string-utils.js
@@ -1,3 +1,19 @@
 export function ucfirst (str) {
   return `${str.substr(0, 1).toUpperCase()}${str.substr(1)}`
 }
+
+export const PolishBreakingText = () => {
+  function lastSingleLetterToNewLine(el) {
+    let result;
+    el.forEach((element, i) => {
+       
+      if(!element.innerHTML.match(/[{}]|<script|^\n$/gi)){
+        result = element.innerHTML.replace(/ (.) /gi, " "+'\$1'+"&nbsp;");
+      }
+      element.innerHTML = result;
+      //console.log(result);
+    });
+  }
+  let el = document.querySelectorAll('h1, h2, h3, h4, h5,, h6, p, span, em, .text, .paragraph');
+  lastSingleLetterToNewLine(el);  
+}
