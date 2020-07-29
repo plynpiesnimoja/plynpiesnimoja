26f56b64d24ca2a75b9ab5b97c397b3b57284da2
dominicom <dominik.kiepuszewski@gmail.com>
2020-07-29 16:48:08 +0200

    Added favicons and logo files, setup manifest

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,21 +11,74 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   plugins: [
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-173147850-1',
+        trackingId: `UA-173147850-1`,
         // this option places the tracking script into the head of the DOM
         head: true,
         // other options
       },
     },
-    'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    // 'gatsby-plugin-breakpoints',
-    'gatsby-plugin-root-import',
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Płyń pieśni moja`,
+        short_name: `Płyń pieśni moja`,
+        start_url: `/`,
+        icon: `src/logo.png`,
+        icons: [
+          {
+            'src': `icons/icon-48x48.png`,
+            'sizes': `48x48`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-72x72.png`,
+            'sizes': `72x72`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-96x96.png`,
+            'sizes': `96x96`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-144x144.png`,
+            'sizes': `144x144`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-192x192.png`,
+            'sizes': `192x192`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-256x256.png`,
+            'sizes': `256x256`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-384x384.png`,
+            'sizes': `384x384`,
+            'type': `image/png`
+          },
+          {
+            'src': `icons/icon-512x512.png`,
+            'sizes': `512x512`,
+            'type': `image/png`
+          }
+        ],
+        background_color: `#3c0323`,
+        theme_color: `#fee2bd`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-root-import`,
+    {
+      resolve: `gatsby-source-sanity`,
       options: {
         ...clientConfig.sanity,
         token,
@@ -34,9 +87,9 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-plugin-html-attributes',
+      resolve: `gatsby-plugin-html-attributes`,
       options: {
-        lang: 'pl'
+        lang: `pl`
       }
     }
   ]
