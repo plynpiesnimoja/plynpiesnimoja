d11af7f66f4e045aad50d436670154c0c07cbe9b
dominicom <dominik.kiepuszewski@gmail.com>
2020-08-27 12:50:29 +0200

    Created new input component for Youtube movie ID with id extractor from url, works fine but console prints some errors

--- studio/schemas/components/input-youtube-id.js
+++ studio/schemas/components/input-youtube-id.js
@@ -1,27 +1,130 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, useState } from 'react'
+// import { withDocument } from 'part:@sanity/form-builder'
 import FormField from 'part:@sanity/components/formfields/default'
 import PatchEvent, {set, unset} from 'part:@sanity/form-builder/patch-event'
 import getYouTubeID from 'get-youtube-id'
 
-const CheckYouTubeId = value => {
-  let id = getYouTubeID(value)
-  console.log("waljuł from field", value)
-  console.log("Futejdż ajdik", id)
-  return id
+import './input-youtube-id.css'
+// import styles from './input-youtube-id.module.scss'
+
+const styles = {
+  input: { 
+    display: `flex`,
+    width: `calc(100% - 1.25rem)`,
+    height: `2rem`,
+    padding: `0 0.5rem`
+  },
+  preview: {
+    container: 
+    { 
+      display: `flex`,
+      justifyContent: `center`,
+      alignItems: `center`,
+      width: `100%`,
+      minHeight: `10rem`,
+      backgroundColor: `#eaeaea`,
+      marginTop: `1rem`
+    },
+    emptyState: {
+      padding: `1rem`
+    }
+  },
+  extractor: {
+    input: {
+      display: `flex`,
+      width: `calc(100% - 1.25rem)`,
+      height: `2rem`,
+      padding: `0 0.5rem`,
+      backgroundColor: `#a8a8a8`,
+      border: 0
+    },
+    output: {
+      width: `calc(100% - 1.25rem)`,
+      padding: `0.5rem`,
+      height: `2rem`,
+      backgroundColor: `#fff`
+    }
+  }
 }
 
+const ExtractYouTubeId = value => {
+  const [input, setInput] = useState();
+
+  return (
+    <div className="CheckYoutubeid">
+      <ExtractYouTubeIdInput
+        onChange={(event) => setInput(event.target.value)}
+        placeholder='Wpisz adres url filmu z Youtube'
+      />
+      {input && <ExtractYouTubeIdOutput source={input} />}
+    </div>
+  );
+}
+
+const ExtractYouTubeIdInput = ({ value, onChange, ...others }) => {
+  return (
+    <div>
+      <input style={styles.extractor.input} onChange={onChange} value={value} {...others} />
+    </div>
+  );
+};
+
+const ExtractYouTubeIdOutput = ({ source }) => {
+  const id = getYouTubeID(source, { fuzzy: true })
+  return (
+    <div>
+      <div style={styles.extractor.output}>{id ? id : 'Oj! Coś jest nie tak...'}</div>
+    </div>
+  );
+};
+
+
+
+export const YouTubePreview = ({ value }) => {
+  if (value === '') {
+    return (
+      <div style={styles.preview.container}>
+        <div style={styles.preview.emptyState}>
+          <p>Wpisz ID filmu, np. "kLsER_zHiS4".</p>
+          <p>Jeżeli film przykładowo ma adres <em>https://www.youtube.com/watch?time_continue=1&v=<strong>kLsER_zHiS4</strong>&feature=emb_title</em>,
+          to ID filmu są te pogrubione kluczowe 11 znaków.</p>
+          <p>PS. Jeżeli ID będzie poprawny, to automatycznie odtworzy się podgląd filmu.</p>
+          <ExtractYouTubeId />
+        </div>
+      </div>
+    )
+  } else {
+    return(
+      <div style={styles.preview.container}>
+        <iframe
+          width="256" 
+          height="144"
+          src={`https://www.youtube.com/embed/${value}?autoplay=1`} 
+          frameborder="0" 
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        />
+      </div>
+    )
+  }
+}
+
+
+
+// https://christianlobaugh.com/blog/2020/01/adding-embedded-video-to-our-new-gatsby-blog/
 // https://www.sanity.io/guides/how-to-make-a-custom-input-component
 
-const createPatchFrom = value => PatchEvent.from(value === '' ? unset() : set(CheckYouTubeId(value)))
+const createPatchFrom = value => PatchEvent.from(value === '' ? unset() : set(value))
 
-export default class InputYoutubeId extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {value: ''};
+class InputYoutubeId extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
 
-  //   this.handleChange = this.handleChange.bind(this);
-  //   //this.handleSubmit = this.handleSubmit.bind(this);
-  // }
+    //this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   handleChange = (value) => {
     const {onChange} = this.props
@@ -48,21 +151,25 @@ export default class InputYoutubeId extends Component {
     // const {min, max, step} = type.options.range
 
     return (
-      <FormField label={type.title} description={type.description}>
-        <input
-          type="text"
-          // min={min}
-          // max={max}
-          // step={step}
-          value={value === undefined ? '' : value}
-          // onChange={event => {
-          //   onChange(createPatchFrom(event.target.value))
-          // }}
-          onChange={this.handleChange}
-          ref={element => this._inputElement = element}
-        />
-        <div>{this.state.value}</div>
-      </FormField>
+      <>
+        <FormField label={type.title} description={type.description}>
+          <input
+            className="InputYoutubeId"
+            type="text"
+            style={styles.input}
+            value={value === undefined ? '' : value}
+            onChange={event => {
+              onChange(createPatchFrom(event.target.value))
+              this.setState({ value: event.target.value })
+            }}
+            ref={element => this._inputElement = element}
+          />
+        </FormField>
+        <YouTubePreview value={this.state.value} />
+      </>
     )
   }
 }
+
+export default InputYoutubeId
+
--- studio/schemas/objects/youtube.js
+++ studio/schemas/objects/youtube.js
@@ -1,17 +1,24 @@
 import React from 'react'
-
+import InputYoutubeId from '../components/input-youtube-id'
 
 // ID from Youtube URL
 // https://www.sanity.io/guides/portable-text-how-to-add-a-custom-youtube-embed-block
-export const YouTubePreview = ({ value }) => (
-  <iframe
-    width="256" 
-    height="144"
-    src={`https://www.youtube.com/embed/${value.videoId}`} 
-    frameborder="0" 
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-  />
-)
+export const YouTubePreview = ({ value }) => {
+  if (!value) {
+    return <div>Dawaj ID filmu!</div>
+  } else {
+    return(
+      <iframe
+        width="256" 
+        height="144"
+        src={`https://www.youtube.com/embed/${value.videoId}`} 
+        frameborder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      />
+    )
+  }
+}
+
 
 
 export default {
@@ -27,6 +34,7 @@ export default {
         Rule.error('Musisz podać "id" filmu na Youtubie').required(),
         Rule.regex( /([a-z0-9_-]{11})?/gim ).length(11).error('Użyty ID tego video nie jest poprawny, upewnij się, skopiuj i wklej ponownie.').required()
       ],
+      inputComponent: InputYoutubeId,
       options: {
         isHighlighted: true
       }
